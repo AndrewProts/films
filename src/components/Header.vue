0ed874a4d3082ade1945f2fb2258acc9addf001d
AndrewProts <prozandrey13@gmail.com>
2017-10-13 15:39:36 +0300

    git link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
         <router-link to="/shedule">Schedule</router-link>
         <router-link to="/admin">Admin Panel</router-link>
       </div>
-      <a href="#" class="github">
+      <a href="https://github.com/AndrewProts/films" target="blank" class="github">
         <img src="https://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="Github">
       </a>
     </div>
